3c24b2179f9317d5a0044b49e3039d1a4a94e25d
Kurt Gardiner <k.a.gardiner@gmail.com>
2017-07-11 17:26:21 +1000

    :flag-tk: Adds Pavement washing

--- lib/bundler-test-gem2.rb
+++ lib/bundler-test-gem2.rb
@@ -1,5 +1,11 @@
 require 'bundler-test-gem2/does-things'
 
 module BundlerTestGem2
-  VERSION = '0.0.3'
+  VERSION = '0.0.4'
+
+  class Pavement
+    def self.wash
+      Utilities::Water.consume
+    end
+  end
 end
